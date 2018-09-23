49380e25836de57cdb98aa2fa2c6691eacc7ccf1
ANTONIO <joelant97@gmail.com>
2018-09-22 23:24:24 -0400

    Comienzo uso de Templates

--- src/mysite/resources/views.py
+++ src/mysite/resources/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Evaluacion
 
 
 def index(request):
-    return HttpResponse("<h1>Esta es la Home-Pages de las Evaluaciones</h1>")
+    all_evaluaciones = Evaluacion.objects.all()
+    template = loader.get_template('resources/index.html')
+    context = {
+        'all_evaluaciones': all_evaluaciones,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 #Los request conectan las WebPages, Imagenes o lo que sea.
