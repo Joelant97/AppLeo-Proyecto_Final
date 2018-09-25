ba95b5fbd207d754d25e1e79a2d305e196d3726c
ANTONIO <joelant97@gmail.com>
2018-09-24 23:17:27 -0400

    Get Object or 404

--- src/mysite/resources/urls.py
+++ src/mysite/resources/urls.py
@@ -2,13 +2,13 @@
 from django.conf.urls import url
 from . import views
 
-
+app_name = 'resources'
 
 urlpatterns = [
     # /evaluar/
     url(r'^$', views.index, name='index'),
 
-    # /evaluar/1       --> 1 en este caso es el ID de la Evaluacion. (evaluacion_id).
+    # /evaluar/<evaluacion_id>       --> 1 en este caso es el ID de la Evaluacion. (evaluacion_id).
     url(r'^(?P<evaluacion_id>[0-9]+)/$', views.detalles, name='detalles'),     #Por razones de prueba estoy validando los ID solo de 0-9.
 
 ]
--- src/mysite/resources/views.py
+++ src/mysite/resources/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Evaluacion
 
 
@@ -10,15 +10,6 @@ def index(request):
 
 #Los request conectan las WebPages, Imagenes o lo que sea.
 def detalles(request, evaluacion_id):
-    try:
-        evaluacion = Evaluacion.objects.get(pk=evaluacion_id)
-    except Evaluacion.DoesNotExist:
-        raise Http404("La evaluacion no existe o no ha sido encontrada por el Servidor.")
+    evaluacion = get_object_or_404(Evaluacion, pk=evaluacion_id)
     return render(request, 'resources/detalles.html', {'evaluacion': evaluacion})
-
-
-
-
-
-
 
